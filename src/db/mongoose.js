9969d644108d883a39d2aa1c10689861b526b1ca
aaron shivers <aaorn@moonmail.org>
2020-02-17 10:39:30 -0600

    update .gitlab-ci.yml

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,26 +1,15 @@
 const mongoose = require('mongoose')
 
-const {
-  MONGO_DATABASE,
-  MONGO_HOST,
-  MONGODB_URI,
-} = process.env
-
-const uri = `mongodb://${ MONGO_HOST }:27017/${ MONGO_DATABASE }`
-const encodedUri = process.env.NODE_ENV !== 'production' ? MONGODB_URI : encodeURI(uri)
-
 const options = {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
-  dbName: MONGO_DATABASE,
 }
 
 const connectToDB = async () => {
   try {
-    const conn = await mongoose.connect(encodedUri, options)
-    console.log(`MongoDB Connected: ${ conn.connection.host } | ${ process.env.MONGO_DATABASE }`)
-
+    const conn = await mongoose.connect(process.env.MONGODB_URI, options)
+    console.log(`MongoDB Connected: ${ conn.connection.host }`)
   } catch (error) {
     console.log(error.message)
   }
